4bd09e12b251d649c1408ef1e804e7fce2605388
Hunter Phillips <hunterphillips419@gmail.com>
2022-01-03 16:58:17 -0500

    Add computerPlay method
    Return 'Rock', 'Paper', or 'Scissors'

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,13 @@
     This script contains the functionality for rock, paper, scissors.
 */
 
+/*
+    computerPlay 
+        Randomly returns 'Rock', 'Paper', or 'Scissors'
+*/
+function computerPlay() {
+    // Find a random value between 0 and 3 (exclusive)
+    let random_value = Math.floor(Math.random() * 3);
+    const options = ['Rock', 'Paper', 'Scissors'];
+    return options[random_value]
+}
